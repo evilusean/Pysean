b4fda3803a81c0e80a50eeaca6c82e16cfb3c49d
evilusean <82889521+evilusean@users.noreply.github.com>
2021-12-18 17:16:16 -0400

    Create ScrollGang.bat

---
+++ youtube-dl/ScrollGang.bat
@@ -0,0 +1,16 @@
+@echo off
+cd /D "%~dp0"
+youtube-dl ^
+--restrict-filenames ^
+--ignore-errors ^
+--output "I:/Music/Scroll/%%(playlist_uploader)s-%%(playlist_title)s/%%(title)s.%%(id)s.%%(ext)s" ^
+--download-archive ScrollGang.txt ^
+--format bestaudio/best ^
+--extract-audio ^
+--audio-format mp3 ^
+--audio-quality 160K ^
+--embed-thumbnail ^
+--restrict-filenames ^
+"https://www.youtube.com/c/scroll%E5%BF%8D/videos" ^
+!video!
+PAUSE
