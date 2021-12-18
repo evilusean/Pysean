786b2a150b4d56b995a1076e20c2a8f59ceed403
evilusean <82889521+evilusean@users.noreply.github.com>
2021-12-18 15:07:52 -0400

    Update config.bat

--- youtube-dl/config.bat
+++ youtube-dl/config.bat
@@ -1,4 +1,4 @@
-#downloads channel using config.txt parameters
+#downloads channel using config.txt parameters as video
 cd /D "%~dp0"
 youtube-dl https://www.youtube.com/user/EngineeringExplained/videos --config-location config.txt
 PAUSE
