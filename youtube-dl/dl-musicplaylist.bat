b914f46a787c54ed485c018bfd4dbdf9415894cd
evilusean <82889521+evilusean@users.noreply.github.com>
2021-12-18 15:28:18 -0400

    Create dl-musicplaylist.bat

---
+++ youtube-dl/dl-musicplaylist.bat
@@ -0,0 +1,12 @@
+#downloads an entire playlist as mp3
+@echo off
+cd /D "%~dp0"
+setlocal enableDelayedExpansion
+set /p video="Paste the music playlist link to download: "
+youtube-dl ^
+--config-location config.txt ^
+--output Music/%%(playlist_uploader)s-%%(playlist_title)s/%%(title)s.%%(id)s.%%(ext)s ^
+--format bestaudio/best ^
+--extract-audio ^
+!video!
+PAUSE
