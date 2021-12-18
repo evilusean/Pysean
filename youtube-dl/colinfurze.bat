6e1047f2c0b693158941ccddd9b3097af4f5541b
evilusean <82889521+evilusean@users.noreply.github.com>
2021-12-18 15:04:52 -0400

    Update colinfurze.bat

--- youtube-dl/colinfurze.bat
+++ youtube-dl/colinfurze.bat
@@ -1,4 +1,4 @@
-#downloads entire colinfruze channel in an archive
+#downloads entire colinfruze channel in a video archive
 cd /D "%~dp0"
 youtube-dl https://www.youtube.com/user/colinfurze/videos --format ("bestvideo[width>=1920]"/bestvideo)+bestaudio/best --download-archive archive.txt --output %%(uploader)s/%%(upload_date)s.%%(title)s.%%(id)s.%%(ext)s --restrict-filenames --merge-output-format mkv --ignore-errors
 PAUSE
