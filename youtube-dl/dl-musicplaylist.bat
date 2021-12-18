db382dc539fe5d8228914ac5c79fbb6521520aea
evilusean <82889521+evilusean@users.noreply.github.com>
2021-12-18 16:31:03 -0400

    Update dl-musicplaylist.bat

--- youtube-dl/dl-musicplaylist.bat
+++ youtube-dl/dl-musicplaylist.bat
@@ -5,6 +5,7 @@ setlocal enableDelayedExpansion
 set /p video="Paste the music playlist link to download: "
 youtube-dl ^
 --config-location config.txt ^
+--download-archive archive.txt ^
 --output Music/%%(playlist_uploader)s-%%(playlist_title)s/%%(title)s.%%(id)s.%%(ext)s ^
 --format bestaudio/best ^
 --extract-audio ^
