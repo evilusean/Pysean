764eb394810315c7ea9ed9899805f50366c41437
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-06-23 11:50:52 -0300

    Update MoviePathToGif.py
    works correctly, previous one was saving the large and small files under the same name

--- FFMPEG/MoviePathToGif.py
+++ FFMPEG/MoviePathToGif.py
@@ -1,7 +1,7 @@
 import subprocess
 import os
 
-def movie_to_gif(input_file, start_time, end_time, output_file="output.gif", fps=10, width="max"):
+def movie_to_gif(input_file, start_time, end_time, output_file, fps=10, width="max"):
   """
   Converts a section of a movie to a GIF using ffmpeg.
 
@@ -25,13 +25,12 @@ def movie_to_gif(input_file, start_time, end_time, output_file="output.gif", fps
   else:
     width = int(width)  # type the width of the size of gif you want, Ex: 640, 1280 etc
 
-  # Check if output files exist, if so, increment the name
-  for output_file in [output_gif, output_gif2]:
-    base_name, ext = os.path.splitext(output_file)
-    counter = 1
-    while os.path.exists(output_file):
-      output_file = f"{base_name}_{counter}{ext}"
-      counter += 1
+  # Check if output file exists, if so, increment the name
+  base_name, ext = os.path.splitext(output_file)
+  counter = 1
+  while os.path.exists(output_file):
+    output_file = f"{base_name}_{counter}{ext}"
+    counter += 1
 
   # Construct the ffmpeg command
   command = [
@@ -55,13 +54,14 @@ start_time = 31  # Start at 10 seconds
 end_time = 35  # End at 20 seconds
 output_gif = "/home/sean/Pictures/gif/Cyberpunk.gif"
 output_gif2 = "/home/sean/Pictures/gif/Cyberpunk_full.gif"
-gif_fps = 5  # Set the GIF frame rate to 5 fps
+gif_fps = 10  # Set the GIF frame rate to 5 fps
 gif_width = 640  # will save the first gif as 640 (for cell phones/whatever)
 gif_width2 = "max"  # type "max" to automatically set the width of the saved gif to the same size as movie, 
 #type an int to use another size
 
 movie_to_gif(input_movie, start_time, end_time, output_gif, gif_fps, gif_width)  # will save first small gif
 movie_to_gif(input_movie, start_time, end_time, output_gif2, gif_fps, gif_width2)  # will save full size gif
+
 
 
 
