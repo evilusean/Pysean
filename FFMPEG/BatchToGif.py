17b791bfc8b163609899615f94392c284e132787
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-06-24 05:28:05 -0300

    Update BatchToGif.py

--- FFMPEG/BatchToGif.py
+++ FFMPEG/BatchToGif.py
@@ -3,7 +3,13 @@ import os
 
 def movie_to_gif(input_file, start_time, end_time, output_file, fps=10, width="max"):
   """
-  Converts a section of a movie to a GIF using ffmpeg.
+    Everytime you load a new episode update 3 variables: 'input_movie', 'episode', and 'timestamps_file'
+  
+    Below Code to create a new Timestamp file in correct folder from terminal:
+    cd /media/sean/40F47947F4794068/MEmes/Timestamps/CyberpunkEdgerunners
+    touch S01E03Stamps.txt
+
+      Converts a section of a movie to a GIF using ffmpeg.
 
   Args:
     input_file: Path to the input movie file.
@@ -100,18 +106,19 @@ def time_to_seconds(time_str):
 
 # Example usage:
 #input_movie = "/home/sean/Downloads/movie_1.mp4"
-input_movie = "/media/sean/D80477BF04779EE6/CyberpunkEdgerunners/Cyberpunk - Edgerunners - S01E02 - Like A Boy.mkv"
+input_movie = "/media/sean/D80477BF04779EE6/CyberpunkEdgerunners/Cyberpunk - Edgerunners - S01E03 - Smooth Criminal.mkv"
 gif_fps = 15  # Set the GIF frame rate FPS
 gif_width = 640  # will save the first gif as 640 (for cell phones/whatever)
 gif_width2 = "max"  # type "max" to automatically set the width of the saved gif to the same size as movie, 
 
 # Read timestamps from a file
-timestamps_file = "/media/sean/40F47947F4794068/MEmes/Timestamps/CyberpunkEdgerunners/S01E02Stamps.txt"  # Replace with your actual file path
+episode = "S01E03" #replace with current episode
+timestamps_file = "/media/sean/40F47947F4794068/MEmes/Timestamps/CyberpunkEdgerunners/S01E03Stamps.txt"  # Replace with your actual file path
 timestamps = read_timestamps_from_file(timestamps_file)
 
 # Create GIFs for each timestamp
 for i, (start_time, end_time) in enumerate(timestamps):
-  output_gif = f"/media/sean/MusIX/Pics/Gifs/Cyberpunk_{i+1}_small.gif"  # Generate unique output file names
-  output_gif2 = f"/media/sean/MusIX/Pics/Gifs/Cyberpunk_{i+1}_full.gif"  # Generate unique output file names
+  output_gif = f"/media/sean/MusIX/Pics/Gifs/Cyberpunk_{episode}_{i+1}_small.gif"  # Generate unique output file names
+  output_gif2 = f"/media/sean/MusIX/Pics/Gifs/Cyberpunk_{episode}_{i+1}_full.gif"  # Generate unique output file names
   movie_to_gif(input_movie, start_time, end_time, output_gif, gif_fps, gif_width)
   movie_to_gif(input_movie, start_time, end_time, output_gif2, gif_fps, gif_width2)
