80e176a261c7ba9a8a5abe88f8e5cf1a75891e1f
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-07-27 20:46:56 -0300

    Update English-Slovak.py

--- Sean/Slovak/PiperTTS/English-Slovak.py
+++ Sean/Slovak/PiperTTS/English-Slovak.py
@@ -4,8 +4,10 @@ import piper
 #going to see if there is an easier way to do this, instead of manually translating 1,000 + lines of slovak and creating a new variable for each line x2
 #https://github.com/rhasspy/piper
 #I could just create a bunch of files, one set in english, 
-#one set in slovak(use it for anki, add an incrementing number automatically update title to the input name, have it automatically save the file as '.mp3' (does '.wav' work?),
-#save to a CSV with a link to .mp3 and english translation, and then combine them all into one file using ffmpeg or something, and add each incremented filename to a big file + english translations - need to think
+#one set in slovak(use it for anki, add an incrementing number automatically update title to the input name,
+#   have it automatically save the file as '.mp3' (does '.wav' work?),
+#save to a CSV with a link to .mp3 and english translation, and then combine them all into one file using ffmpeg or something, 
+#   and add each incremented filename to a big file + english translations - need to think
 
 # Define the text in both languages
 english_text = "Hello, world!"
@@ -13,7 +15,7 @@ slovak_text = "Dobrý deň!"
 
 # Specify the voices for each language
 english_voice = "en_US-lessac-medium"
-slovak_voice = "sk_SK-lili-medium"  # Assuming you have a Slovak model named 'sk_SK-lili-medium'
+slovak_voice = "sk_SK-lili-medium" 
 
 # Create a list of tuples containing the text and voice for each language
 language_data = [
