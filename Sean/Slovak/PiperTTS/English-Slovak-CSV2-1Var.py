c31a4f221eb241adec1a155a343a394d57fe52d1
Sean Teams <82889521+evilusean@users.noreply.github.com>
2024-07-28 14:04:44 -0300

    Colors

--- Sean/Slovak/PiperTTS/English-Slovak-CSV2-1Var.py
+++ Sean/Slovak/PiperTTS/English-Slovak-CSV2-1Var.py
@@ -108,7 +108,7 @@ def process_csv(category):
     print(f"Translation and audio synthesis complete for {category}!")
 
 # Example usage:
-category = "Adjectives"  # Replace with your directory / CSV name (must be the same)
+category = "Colors"  # Replace with your directory / CSV name (must be the same)
 process_csv(category)
 
 # Commented out the ffmpeg command to combine audio files
