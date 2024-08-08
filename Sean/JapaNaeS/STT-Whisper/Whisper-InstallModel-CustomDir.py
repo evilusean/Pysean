b906a127f3f7415d5cedf15dc2751d65fd709bc6
Sean Teams <shawntemesy@hotmail.com>
2024-08-08 19:42:00 -0300

    Update Whisper-InstallModel-CustomDir.py

--- Sean/JapaNaeS/STT-Whisper/Whisper-InstallModel-CustomDir.py
+++ Sean/JapaNaeS/STT-Whisper/Whisper-InstallModel-CustomDir.py
@@ -1,3 +1,3 @@
 #will attempt to download and install whispers large model to a custom directory with more storage space instead of default SSD
 import whisper
-model = whisper.load_model('large', download_root='/media/sean/MusIX/OpenAI-Whisper/pip')
+model = whisper.load_model('medium', download_root='/mnt/4e1c82f2-2f37-4698-9019-ee96f71a51ca/Lang/OpenAI-Whisper')
