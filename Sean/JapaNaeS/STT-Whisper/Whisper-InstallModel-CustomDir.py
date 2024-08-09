6f30be96a925cf4c5b16ee282c4e70063de9dfbd
Sean Teams <shawntemesy@hotmail.com>
2024-08-09 00:47:45 -0300

    creating new verion
    need to test out the new model

--- Sean/JapaNaeS/STT-Whisper/Whisper-InstallModel-CustomDir.py
+++ Sean/JapaNaeS/STT-Whisper/Whisper-InstallModel-CustomDir.py
@@ -1,3 +1,8 @@
 #will attempt to download and install whispers large model to a custom directory with more storage space instead of default SSD
 import whisper
 model = whisper.load_model('medium', download_root='/mnt/4e1c82f2-2f37-4698-9019-ee96f71a51ca/Lang/OpenAI-Whisper')
+
+
+
+
+
