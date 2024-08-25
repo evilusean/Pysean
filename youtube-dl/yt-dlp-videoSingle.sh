b685d5417b300727511da7eaedceb2a53e9181a7
Sean Teams <shawntemesy@hotmail.com>
2024-08-25 13:12:41 -0300

    Update yt-dlp-videoSingle.sh
    changed quality, 4 gigs for a video is excessive

--- youtube-dl/yt-dlp-videoSingle.sh
+++ youtube-dl/yt-dlp-videoSingle.sh
@@ -6,6 +6,9 @@ video_url="https://www.youtube.com/watch?v=dIu42GX9cqo"
 # Output folder
 output_folder="/mnt/sdb3/Videos/Youtube"
 
+# Video quality (adjust this variable for different qualities)
+video_quality="720p"
+
 # Check if both arguments are provided
 if [ -z "$video_url" ] || [ -z "$output_folder" ]; then
   echo "Usage: $0 <video_url> <output_folder>"
@@ -15,9 +18,9 @@ fi
 # Extract video ID from the URL
 video_id=$(youtube-dl --get-id "$video_url")
 
-# Download the video with audio and visual
+# Download the video with audio and visual, specifying the quality
 yt-dlp -o "$output_folder/%(title)s.%(ext)s" \
-  --format bestvideo+bestaudio \
+  --format "bestvideo[height<=${video_quality}]+bestaudio" \
   "$video_url"
 
 echo "Downloaded video to: $output_folder"
