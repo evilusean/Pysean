74dbc7cc1926170ecec1799c5999cc8e7302af43
Sean Teams <shawntemesy@hotmail.com>
2024-08-29 15:24:42 -0300

    -

--- youtube-dl/yt-dlp-musicSingle.sh
+++ youtube-dl/yt-dlp-musicSingle.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Input YouTube video URL
-video_url="https://www.youtube.com/watch?v=_JEKDJoqA8s"
+video_url="https://www.youtube.com/watch?v=uc7qgWVeJc8"
 
 # Output folder
 output_folder="/mnt/sdb2/Media/Music/1S1ngles"
