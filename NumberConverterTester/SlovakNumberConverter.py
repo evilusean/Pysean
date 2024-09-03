66439dacb9360e4ede8ed056d3b5afa8cf0df41e
Sean Teams <shawntemesy@hotmail.com>
2024-09-02 23:12:08 -0300

    renamed to .py - added 2 boilerplate
    might need to redo in JS later on to have a better UI/UX, but can at least get the logic done in python and then switch over

---
+++ NumberConverterTester/SlovakNumberConverter.py
@@ -0,0 +1,2 @@
+#A file for creating a Slovak number convertor to test myself, will need range and conversion from
+#will need an input range, larger numbers are harder, but still might need to practice 1-100, etc
