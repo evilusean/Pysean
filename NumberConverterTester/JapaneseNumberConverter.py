a8638e7aeafda751ae57a87db299ebb883cfa8da
Sean Teams <shawntemesy@hotmail.com>
2024-09-19 02:47:29 -0300

    -
    new audio, need mp3s for anki deck

--- NumberConverterTester/JapaneseNumberConverter.py
+++ NumberConverterTester/JapaneseNumberConverter.py
@@ -1,4 +1,4 @@
 #A file for creating a japanese number convertor to test myself, will need range and conversion from
-# numeric to written japanese both hiragana, kanji, and romaji(English) versions, remeber that Japanese is weird
+# numeric to written japanese both hiragana, kanji, and romaji(English) versions, remember that Japanese is weird
 #it starts at 4 digits instead of 3 for man, etc,  so will need special rules, 
 #will need an input range, larger numbers are harder, but still might need to practice 1-100, etc
