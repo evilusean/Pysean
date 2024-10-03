c5947408359776fed682b474a2a4e550e2c1f63c
Sean Teams <shawntemesy@hotmail.com>
2024-10-03 19:22:57 -0300

    Update yt-dlp-musicSingle.sh

--- youtube-dl/yt-dlp-musicSingle.sh
+++ youtube-dl/yt-dlp-musicSingle.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Input YouTube video URL
-video_url="https://www.youtube.com/watch?v=nC3ueONMVbc&ab_channel=REZZ"
+video_url="https://www.youtube.com/watch?v=0dVaZ-2Sj1k&ab_channel=OneTrueGod"
 
 # Output folder
 output_folder="/mnt/sdb2/Media/Music/1S1ngles"
