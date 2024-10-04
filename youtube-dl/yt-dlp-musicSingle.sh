0b32d8c4453380f49bfe12a34d3b9a9b03bd5cee
Sean Teams <shawntemesy@hotmail.com>
2024-10-03 21:07:50 -0300

    Update yt-dlp-musicSingle.sh

--- youtube-dl/yt-dlp-musicSingle.sh
+++ youtube-dl/yt-dlp-musicSingle.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Input YouTube video URL
-video_url="https://www.youtube.com/watch?v=0dVaZ-2Sj1k&ab_channel=OneTrueGod"
+video_url="https://www.youtube.com/watch?v=YoeYUynoCTc&ab_channel=%24uicideboy%24"
 
 # Output folder
 output_folder="/mnt/sdb2/Media/Music/1S1ngles"
