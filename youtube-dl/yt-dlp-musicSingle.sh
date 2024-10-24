5c6f7d304b982caa4cd4985f8fdf20c11321b2c1
Sean Teams <shawntemesy@hotmail.com>
2024-10-23 21:38:07 -0300

    Update yt-dlp-musicSingle.sh

--- youtube-dl/yt-dlp-musicSingle.sh
+++ youtube-dl/yt-dlp-musicSingle.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Input YouTube video URL
-video_url="https://www.youtube.com/watch?v=rA1hBL_-oZc&ab_channel=RoyalBlood"
+video_url="https://www.youtube.com/watch?v=7Wsin5LH5sM&ab_channel=KidCudi-Topic"
 
 # Output folder
 output_folder="/mnt/sdb2/Media/Music/1S1ngles"
