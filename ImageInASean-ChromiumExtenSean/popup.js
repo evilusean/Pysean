f77bf7dd02dc88555c2bf7846bb48ed424b5bd0b
Sean Teams <82889521+evilusean@users.noreply.github.com>
2025-02-18 05:41:58 -0400

    Update popup.js

--- ImageInASean-ChromiumExtenSean/popup.js
+++ ImageInASean-ChromiumExtenSean/popup.js
@@ -1,8 +1,10 @@
-document.getElementById('saveImages').addEventListener('click', () => {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      chrome.tabs.sendMessage(tabs[0].id, { action: "getImages" }, (response) => {
-        console.log("Received image URLs from content script:", response.urls);
-        chrome.runtime.sendMessage({ action: "downloadImages", urls: response.urls });
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('saveImages').addEventListener('click', () => {
+      chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        chrome.tabs.sendMessage(tabs[0].id, { action: "getImages" }, (response) => {
+          console.log("Received image URLs from content script:", response.urls);
+          chrome.runtime.sendMessage({ action: "downloadImages", urls: response.urls });
+        });
       });
     });
   });
