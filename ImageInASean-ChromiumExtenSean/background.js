6106234a63d9597296bac873429d998a66e598a8
Sean Teams <82889521+evilusean@users.noreply.github.com>
2025-02-18 05:12:39 -0400

    Update background.js

--- ImageInASean-ChromiumExtenSean/background.js
+++ ImageInASean-ChromiumExtenSean/background.js
@@ -1,7 +1,11 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.action === "downloadImages") {
       message.urls.forEach(url => {
-        chrome.downloads.download({ url: url });
+        const filename = url.split('/').pop(); // Extract the filename from the URL
+        chrome.downloads.download({
+          url: url,
+          filename: `4chan_images/${filename}` // Save to a folder named '4chan_images'
+        });
       });
     }
   });
