9490bb0ae49d647b98e8ba51879f7aa3db893c6c
Sean Teams <82889521+evilusean@users.noreply.github.com>
2025-02-19 11:16:19 -0400

    Update background.js

--- ImageInASean-ChromiumExtenSean/background.js
+++ ImageInASean-ChromiumExtenSean/background.js
@@ -2,18 +2,20 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.action === "downloadImages") {
     console.log("Received downloadImages message with URLs:", message.urls);
     message.urls.forEach(url => {
-      const filename = url.split('/').pop(); // Extract the filename from the URL
+      const filename = url.split('/').pop();
       console.log("Attempting download for:", url, "as", filename);
       chrome.downloads.download({
         url: url,
-        filename: `/mnt/sdb4/MEmes/4Chan-Unsorted/${filename}` // Save to the specified folder
+        filename: `4Chan-Unsorted/${filename}`,
+        conflictAction: 'uniquify'
       }, (downloadId) => {
         if (chrome.runtime.lastError) {
-          console.error("Download failed:", chrome.runtime.lastError);
+          console.error("Download failed:", chrome.runtime.lastError.message);
         } else {
           console.log("Download started with ID:", downloadId);
         }
       });
     });
   }
+  return true;
 });
