f558b58727920fad8d7a4dafad9293a37a5738e0
Sean Teams <82889521+evilusean@users.noreply.github.com>
2025-06-06 09:56:03 -0300

    Update yt-dlp-musicSingle.sh

--- youtube-dl/yt-dlp-musicSingle.sh
+++ youtube-dl/yt-dlp-musicSingle.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Input YouTube video URL
-video_url="https://www.youtube.com/watch?v=fvEZ7OGwitU"
+video_url="https://www.youtube.com/watch?v=9jJgbpEl7UQ"
 
 # Output folder
 output_folder="/mnt/sdb2/Media/Music/1S1ngles"
