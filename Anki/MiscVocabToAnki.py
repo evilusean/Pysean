875923e671d475dd1b74258b666a120230037166
Sean Teams <82889521+evilusean@users.noreply.github.com>
2025-07-13 11:48:36 -0300

    Create MiscVocabToAnki.py

---
+++ Anki/MiscVocabToAnki.py
@@ -0,0 +1,8 @@
+# TO DO : Create a script that creates anki decks/cards from my vocab lists - formats the first line of the kanji to be large and easily readable, 
+# since anki uses html for formatting, this should be as easy as using inline CSS, or setting the card format beforehand,
+# so the 'Kanji', 'Hiragana', and 'Romaji' on the front, 
+# and puts the English meaning/definition text after the last '=' equals sign on the back of the cards
+# some cards are missing the '=' inbetween the kanji=hiragana=romaji, so either I can get AI to reformat cards, 
+# or make the script only check for the last '=' sign and put everything before it on the front and everything after it on the back
+# also remember I added some lines that have 'Example = .....' that will need to be fixed
+# my first set of misc vocab is almost at 1000 lines, I do NOT want to type that out manually creating each new anki card
